f80db45c1da1fe9dadca2d43ae4c514e8ce52bbd
aletgn <alessandro.tognan@gmail.com>
2026-02-08 14:02:21 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="testfooproject",
-    version="0.0.5",
+    version="0.0.8",
     description="TEST",
     long_description=long_description,
     long_description_content_type="text/markdown",
