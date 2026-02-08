5d574cbbaa4d3110d090e1ab58c7c8c1fe2a9bae
Alessandro Tognan <83232805+aletgn@users.noreply.github.com>
2026-02-08 11:23:59 +0100

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="testfooproject",
-    version="release/0.0.0rc0",
+    version="0.0.0",
     description="TEST",
     long_description=long_description,
     long_description_content_type="text/markdown",
